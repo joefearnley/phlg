7cd920d2b4bac83ed6b67c4820b4f6f601f3a268
Joe Fearnley <joe.fearnley@gmail.com>
2013-01-30 22:24:08 -0500

    Adding tests directory and initial api tests.

---
+++ tests/ApiTest.php
@@ -0,0 +1,39 @@
+<?php
+
+class ApiText extends PHPUnit_Framework_TestCase
+{
+  public function testIndex()
+  {
+    $this->assert(false);
+  }
+
+  public function testPostMessage()
+  {
+    $this-assert(false);
+  }
+
+  public function testPostInfoMessage()
+  {
+    $this->assert(false);
+  }
+
+  public function testPostErrorMessage()
+  {
+    $this->assert(false);
+  }
+
+  public function testGetAllMessages()
+  {
+    $this->assert(false);
+  }
+
+  public function testGetMessage()
+  {
+    $this->assert(false);
+  }
+
+  public function testError()
+  {
+    $this->assert(false);
+  }
+}
