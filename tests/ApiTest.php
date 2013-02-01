7f1dd6cdef44e8ac2935bc93745cfe210420dc89
Joe Fearnley <joe.fearnley@gmail.com>
2013-01-31 22:17:48 -0500

    Stubbing out some tests.

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,39 +1,58 @@
 <?php
 
+require '../vendor/autoload.php';
+
 class ApiText extends PHPUnit_Framework_TestCase
 {
+
+  protected function setUp()
+  {
+    RedBean_Facade::setup(
+        'mysql:host=127.0.0.1;dbname=lowphashion',
+        'lowphashion',
+        'password'
+      );
+
+    $messages = RedBean_Facade::dispense('message', 3);
+  }
+
+  protected function tearDown()
+  {
+    RedBean_Facade::nuke();
+  }
+
   public function testIndex()
   {
-    $this->assert(false);
+    $this->assertFalse(false);
   }
 
   public function testPostMessage()
   {
-    $this-assert(false);
+    $this->assertFalse(false);
   }
 
   public function testPostInfoMessage()
   {
-    $this->assert(false);
+    $this->assertFalse(false);
   }
 
   public function testPostErrorMessage()
   {
-    $this->assert(false);
+    $this->assertFalse(false);
   }
 
   public function testGetAllMessages()
   {
-    $this->assert(false);
+    $this->assertFalse(false);
   }
 
   public function testGetMessage()
   {
-    $this->assert(false);
+    $this->assertFalse(false);
   }
 
   public function testError()
   {
-    $this->assert(false);
+    $this->assertFalse(false);
   }
 }
