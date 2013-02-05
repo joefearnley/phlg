161f03b1f971376160b1e0b59d2611d09fced97d
Joe Fearnley <joe.fearnley@gmail.com>
2013-02-04 20:49:58 -0500

    Removing old config file.

--- config/Config.php
+++ config/Config.php
@@ -1,33 +1,24 @@
 <?php
 
-/*
 $config = array(
-  'test' => array(
-    'url' => 'http://localhost/lowphashion',
-    'db_host' => 'localhost',
-    'db_name' => 'lowphashion_test',
-    'db_user' => 'lowphashion',
-    'db_pass' => 'password'
-  ),
   'prod' => array(
-    'url' => 'http://localhost/lowphashion',
-    'db_host' => 'localhost',
+    'host' => 'http://localhost',
+    'uri' => 'lowphashion',
+    'app_name' => 'lowphashion',
+    'db_host' => '127.0.0.1',
     'db_name' => 'lowphashion',
     'db_user' => 'lowphashion',
-    'db_pass' => 'password'
+    'db_pass' => 'password',
+    'connection' => 'mysql:host=127.0.0.1;dbname=lowphashion'
+  ),
+  'test' => array(
+    'host' => 'http://localhost',
+    'uri' => 'lowphashion',
+    'app_name' => 'lowphashion',
+    'db_host' => '127.0.0.1',
+    'db_name' => 'lowphashion_test',
+    'db_user' => 'lowphashion',
+    'db_pass' => 'password',
+    'connection' => 'mysql:host=127.0.0.1;dbname=lowphashion_test'
   )
 );
-*/
-
-class Config
-{
-  const HOST = 'http://localhost';
-  const URI = '/lowphashion';
-
-  const APPNAME = 'lowphashion';
-
-  const DBHOST = '127.0.0.1';
-  const DBNAME = 'lowphashion';
-  const DBUSER = 'lowphashion';
-  const DBPASS = 'password';
-}
