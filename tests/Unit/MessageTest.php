2aaa8b6784af6f568a82f97d32a1e79f2204bb9f
Joe Fearnley <joe.fearnley@gmail.com>
2017-11-17 10:00:38 -0500

    Writing a couple more basic tests

--- tests/Unit/MessageTest.php
+++ tests/Unit/MessageTest.php
@@ -14,31 +14,60 @@ class MessageTest extends TestCase
     /** @test */
     public function it_shows_an_error_message()
     {
-        $app = factory(Application::class)->create(['name' => 'mobile app']);
+        $application = factory(Application::class)->create(['name' => 'mobile app']);
+
         factory(Message::class)->create([ 
-            'application_id' => $app->id,
+            'application_id' => $application->id,
             'body' => 'This is an error',
             'type' => 'error'
         ]);
 
-        // factory(Message::class)->create([ 
-        //     'app_id' => $app->id,
-        //     'body' => 'This is some info',
-        //     'type' => 'info'
-        // ]);
-
-        // factory(Message::class)->create([ 
-        //     'app_id' => $app->id,
-        //     'body' => 'This is some info',
-        //     'type' => 'info'
-        // ]);
-
         $this->get('/messages')
             ->assertStatus(200)
             ->assertJsonFragment([ 
-                'application_id' => "$app->id",
+                'application_id' => "$application->id",
                 'body' => 'This is an error',
                 'type' => 'error'
+            ]);
+    }
+
+    /** @test */
+    public function it_shows_an_info_message()
+    {
+        $application = factory(Application::class)->create(['name' => 'mobile app']);
+
+        factory(Message::class)->create([ 
+            'application_id' => $application->id,
+            'body' => 'This is some info',
+            'type' => 'info'
+        ]);
+
+        $this->get('/messages')
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'application_id' => "$application->id",
+                'body' => 'This is some info',
+                'type' => 'info'
+            ]);
+    }
+
+    /** @test */
+    public function it_show_a_warning_message()
+    {
+        $application = factory(Application::class)->create();
+
+        factory(Message::class)->create([
+            'application_id' => $application->id,
+            'body' => 'This is a warning',
+            'type' => 'warning'
+        ]);
+
+        $this->get('/messages')
+            ->assertStatus(200)
+            ->assertJsonFragment([
+                'application_id' => "$application->id",
+                'body' => 'This is a warning',
+                'type' => 'warning'
             ]);
     }
 }
