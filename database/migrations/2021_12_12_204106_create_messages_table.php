8887a31942c0f94fdbf1cbbb8e230489a5131434
Joe Fearnley <joe.fearnley@gmail.com>
2021-12-12 15:44:15 -0500

    Setting up initial data models

---
+++ database/migrations/2021_12_12_204106_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('application_id');
+            $table->string('body', 256);
+            $table->string('level', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
