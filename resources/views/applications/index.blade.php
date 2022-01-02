5174b64272ec34365eb0101bcde4564b64fc1e28
Joe Fearnley <joe.fearnley@gmail.com>
2022-01-02 09:55:10 -0500

    Adding dashboard application page

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -11,10 +11,15 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="my-6 p-3 border-b">
             <div class="flex flex-row flex-wrap">
-                <div class="w-full">
+                <div class="w-full md:w-3/4">
                     <h3 class="font-bold text-xl mb-3">{{ $application->name }}</h3>
                     <p><strong>Message Count:</strong> {{ $application->messages->count() }}</p>
                     <p><strong>Last Updated:</strong> {{ $application->lastUpdated() }}</p>
+                </div>
+                <div class="w-full md:w-1/4">
+                    <a href="/applications/{{ $application->id}}/edit/">
+                    {{ __('Edit') }}
+                    </a>
                 </div>
             </div>
         </div>
