b977495f41d7c74a9c25f7559d660c83f9d40778
Joe Fearnley <joe.fearnley@gmail.com>
2022-01-03 20:56:49 -0500

    Make add new application button work properly on mobile

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="my-3 max-w-7xl mx-auto">
-            <div class="flex flex-row flex-wrap">
-                <div class="w-full md:w-1/2">
+            <div class="flex flex-row flex-wrap items-center">
+                <div class=" w-1/2">
                     <h2 class="font-semibold text-xl leading-tight">
                         {{ __('Applications') }}
                     </h2>
                 </div>
-                <div class="w-full md:w-1/2 text-right">
+                <div class="w-1/2 text-right">
                     <a href="/applications/create" class="inline-flex items-center px-4 py-2 bg-blue border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring disabled:opacity-25">
                         {{ __('Add New ') }}
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
