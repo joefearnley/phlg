5feba28363dea5a49dbe793304ab37dc394684b4
Joe Fearnley <joe.fearnley@gmail.com>
2022-02-02 21:19:49 -0500

    Moving position of edit button

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="my-3 max-w-7xl mx-auto">
             <div class="flex flex-row flex-wrap justify-between items-center">
-                <div class=" w-1/2">
+                <div class="w-1/2">
                     <h2 class="font-semibold text-xl leading-tight">
                         {{ __('Applications') }}
                     </h2>
@@ -26,15 +26,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="my-6 p-3 border-b">
                 <div class="flex flex-row flex-wrap">
-                    <div class="w-full">
+                    <div class="w-3/4">
                         <h3 class="font-bold text-xl mb-3">
                             {{ $application->name }}
-                            <a href="/applications/{{ $application->id}}/edit/" class="inline-flex items-center px-2 py-2 text-xs">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                                </svg>
-                            </a>
                         </h3>
                         <p>
                             <strong>Message Count:</strong>
@@ -47,6 +41,11 @@
                             @endif
                         </p>
                         <p><strong>Last Updated:</strong> {{ $application->lastUpdated() }}</p>
+                    </div>
+                    <div class="w-1/4">
+                        <a href="/applications/{{ $application->id}}/edit/" class="add-application-button inline-flex items-center px-4 py-2 bg-blue border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring disabled:opacity-25">
+                            Edit
+                        </a>
                     </div>
                 </div>
             </div>
