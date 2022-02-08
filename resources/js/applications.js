4ba11a03cb3a41b4e12a5733248745761d042af9
Joe Fearnley <joe.fearnley@gmail.com>
2022-02-07 21:16:47 -0500

    Trying to create confirmation of application deleting

--- resources/js/applications.js
+++ resources/js/applications.js
@@ -2,6 +2,7 @@
 const addApplicationButton = document.querySelector('.add-application-button');
 const addApplicationForm = document.querySelector('.add-application-form');
 const editApplicationForm = document.querySelector('.edit-application-form');
+const deleteApplicationButton = document.querySelector('.edit-application-form');
 
 window.applicationSwal = Swal.mixin({
     showCancelButton: true,
@@ -20,73 +21,28 @@ window.applicationSwal = Swal.mixin({
       buttonsStyling: false
 });
 
-if (addApplicationButton) {
-    addApplicationButton.addEventListener('click', () => showAddApplicationForm(), false);
+document.addEventListener('click', function(evt) {
+    evt.preventDefault();
 
-        // applicationSwal.fire({
-        //     title: 'Add Application',
-        //     html:addApplicationForm.innerHTML,
-        //     showCloseButton: true,
-        //     showCancelButton: true,
-        //     confirmButtonText: 'Save',
-        //     confirmButtonAriaLabel: 'Save',
-        //     cancelButtonAriaLabel: 'Cancel',
-        //     confirmButtonColor: 'rgba(45, 64, 83, var(--tw-bg-opacity))',
-        //     cancelButtonColor: 'rgba(45, 64, 83, var(--tw-bg-opacity))',
-        //     buttonsStyling: false,
-        //     preConfirm: function() {
-        //         return new Promise((resolve, reject) => {
-        //             // get your inputs using their placeholder or maybe add IDs to them
-        //             resolve({
-        //                 name: addApplicationForm.elements['name'].value
-        //             });
-        //         })
-        //     }
-        // }).then((result) => {
-        //     console.log('submitting form....');
-        //     console.log(result);
-
-        //     if (result.isConfirmed) {
-        //         //submit form
-        //     }
-        // })
-    }
-
-const showAddApplicationForm = async () => {
-    console.log('sadlfkjaslkdjflkajsd');
-
-    const { value: name } = await applicationSwal.fire({
-        title: 'Add Applicaiton',
-        input: 'text',
-        inputLabel: 'Name',
-        inputValue: '',
+    applicationSwal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
         showCancelButton: true,
-        inputValidator: (value) => {
-            if (!value) {
-                return 'Please Enter a Name!'
-            }
+        // confirmButtonColor: '#3085d6',
+        // cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        console.log(result);
+
+        if (result.isConfirmed) {
+            console.log('firing new one...');
+
+            applicationSwal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
         }
     });
-
-    console.log(name);
-
-    // applicationSwal.fire({
-    //     title: 'Add Application',
-    //     html:addApplicationForm.innerHTML,
-    //     showCloseButton: true,
-    //     showCancelButton: true,
-    //     confirmButtonText: 'Save',
-    //     confirmButtonAriaLabel: 'Save',
-    //     cancelButtonAriaLabel: 'Cancel',
-        // confirmButtonColor: 'rgba(45, 64, 83, var(--tw-bg-opacity))',
-        // cancelButtonColor: 'rgba(45, 64, 83, var(--tw-bg-opacity))',
-    //     buttonsStyling: false,
-    // }).then((result) => {
-    //     console.log('submitting form....');
-    //     console.log(result);
-
-    //     if (result.isConfirmed) {
-    //         //submit form
-    //     }
-    // });
-}
+});
