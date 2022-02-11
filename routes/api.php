1501a0e7557fe255a8a3d9953d4b5e0314c7356c
Joe Fearnley <joe.fearnley@gmail.com>
2022-02-10 22:06:48 -0500

    Setting up orion stuff for applciations

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use Orion\Facades\Orion;
+use App\Http\Controllers\Api\ApplicationController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['as' => 'api.'], function() {
+    Orion::resource('applications', Application::class);
 });
