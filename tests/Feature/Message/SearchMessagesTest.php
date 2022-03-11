d87525fbbc35df9894c39cfa7f55e9efd7cf9b00
Joe Fearnley <joe.fearnley@gmail.com>
2022-03-10 21:46:28 -0500

    adding intial message search test

---
+++ tests/Feature/Message/SearchMessagesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class SearchMessagesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
+    public function test_search_messages_with_no_parameters()
+    {
+        $response = $this->actingAs($this->user)
+            ->get(route('messages.index'));
+
+        $response->assertStatus(200);;
+    }
+
+
+}
