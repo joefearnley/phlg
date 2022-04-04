da29bd72df5e4e9d7d507db723f7ec850f63ea64
Joe Fearnley <joe.fearnley@gmail.com>
2022-04-04 08:54:27 -0500

    Add test for search by application and term

--- tests/Feature/Message/SearchMessagesTest.php
+++ tests/Feature/Message/SearchMessagesTest.php
@@ -50,7 +50,9 @@ class SearchMessagesTest extends TestCase
     public function test_search_by_application_shows_application_name()
     {
         $response = $this->actingAs($this->user)
-            ->get(route('messages.index') . '?appid=' . $this->application->id);
+        ->get(route('messages.index', [
+            'appid' => $this->application->id
+        ]));
 
         $response->assertStatus(200)
             ->assertSeeText('Application: ' . $this->application->name);
@@ -70,7 +72,9 @@ class SearchMessagesTest extends TestCase
             ->create();
 
         $response = $this->actingAs($this->user)
-            ->get(route('messages.index') . '?appid=' . $this->application->id);
+        ->get(route('messages.index', [
+            'appid' => $this->application->id
+        ]));
 
         // should show 3 messages from selected application
         $responseData = $response->getOriginalContent()->getData();
@@ -81,8 +85,7 @@ class SearchMessagesTest extends TestCase
             ->assertSeeText($this->messages[1]->body)
             ->assertSeeText($this->messages[2]->body);
 
-        $response->assertStatus(200)
-            ->assertDontSeeText($application2Messages[0]->body)
+        $response->assertDontSeeText($application2Messages[0]->body)
             ->assertDontSeeText($application2Messages[1]->body)
             ->assertDontSeeText($application2Messages[2]->body);
     }
@@ -103,7 +106,9 @@ class SearchMessagesTest extends TestCase
         $searchTerm = $this->messages[0]->body;
 
         $response = $this->actingAs($this->user)
-            ->get(route('messages.index') . '?term=' . $searchTerm);
+        ->get(route('messages.index', [
+            'term' => $searchTerm
+        ]));
 
         $responseData = $response->getOriginalContent()->getData();
         $this->assertEquals(1, $responseData['messages']->count());
@@ -113,5 +118,57 @@ class SearchMessagesTest extends TestCase
             ->assertSee($this->messages[0]->body)
             ->assertDontSeeText($this->messages[1]->body)
             ->assertDontSeeText($this->messages[2]->body);
+    }
+
+    public function test_search_shows_results_for_search_term_and_application()
+    {
+        // already have an application with message on set up, so create a second and one
+        $application2 = Application::factory()->create([
+            'user_id' => $this->user->id
+        ]);
+
+        $application2Messages = Message::factory([
+            'application_id' => $application2->id,
+            'level_id' => 1
+        ])
+            ->count(3)
+            ->create();
+
+        $application3 = Application::factory()->create([
+                'user_id' => $this->user->id
+            ]);
+
+        $application3Messages = Message::factory([
+                'application_id' => $application3->id,
+                'level_id' => 1
+            ])
+                ->count(3)
+                ->create();
+
+        $searchTerm = $this->messages[0]->body;
+
+        $response = $this->actingAs($this->user)
+            ->get(route('messages.index', [
+                'term' => $searchTerm,
+                'appid' => $this->application->id
+            ]));
+
+        $responseData = $response->getOriginalContent()->getData();
+        $this->assertEquals(1, $responseData['messages']->count());
+
+        $response->assertStatus(200)
+            ->assertSeeText('Application: ' . $this->application->name)
+            ->assertSeeText('Search Term: ' . $searchTerm)
+            ->assertSee($this->messages[0]->body);
+
+        // should not see second application's messages
+        $response->assertDontSeeText($application2Messages[0]->body)
+            ->assertDontSeeText($application2Messages[1]->body)
+            ->assertDontSeeText($application2Messages[2]->body);
+
+        // should not see third application's messages
+        $response->assertDontSeeText($application3Messages[0]->body)
+            ->assertDontSeeText($application3Messages[1]->body)
+            ->assertDontSeeText($application3Messages[2]->body);
     }
 }
