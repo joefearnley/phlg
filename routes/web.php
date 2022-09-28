07cf451f29227bb82c13dc58051802a1469d083a
Joe Fearnley <joe.fearnley@gmail.com>
2022-09-28 09:06:04 -0400

    Fixing welcome test

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['guest']);
 
 Route::middleware([
     'auth:sanctum',
--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class WelcomeTest extends TestCase
 {
@@ -19,13 +20,15 @@ class WelcomeTest extends TestCase
             ->assertSee('Sign up');
     }
 
-    public function user_is_redirect_to_dashboard_if_logged_in()
+    public function test_user_is_redirect_to_dashboard_if_logged_in()
     {
         $user = User::factory()->create();
+
+        $this->actingAs($user);
 
         $response = $this->get('/');
 
         $response->assertStatus(302)
-            ->assertRedirect('/dashboard');
+            ->assertRedirect(route('dashboard'));
     }
 }
