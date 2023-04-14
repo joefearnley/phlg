1dc9e6fee63c78dfaf6289dcbbda704c8bec2355
Joe Fearnley <joe.fearnley@gmail.com>
2023-04-13 22:58:13 -0400

    Only who messages for active applications

--- app/Models/User.php
+++ app/Models/User.php
@@ -115,7 +115,7 @@ class User extends Authenticatable
      */
     public function searchMessages($appId = null, $searchTerm = null)
     {
-        $query = $this->messages();
+        $query = $this->messages()->active();
 
         if (!empty($appId)) {
             $query->where('application_id', $appId);
